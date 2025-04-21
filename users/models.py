cb1fdfc345d71c8170bf55cde1dfb58c2895c0f6
achibhossengit <mail.achibhossen@gmail.com>
2025-04-21 08:40:40 +0600

    solve custom user update related issues

--- users/models.py
+++ users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from users.validators import validate_file_type
-from django.core.validators import MinValueValidator
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -16,12 +15,15 @@ class CustomUser(AbstractUser):
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     contact_number = models.CharField(max_length=15, blank=True, null=True)
 
-    REQUIRED_FIELDS = ['email', 'user_type']  # These fields are mandatory
+    REQUIRED_FIELDS = ['email', 'user_type']  # These fields are mandatory for user creations
 
 class Employer(models.Model):
     user = models.OneToOneField(CustomUser, primary_key=True, on_delete=models.CASCADE, related_name='employer')
     company = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.user.username
 
 class JobSeeker(models.Model):
     MALE = 'Male'
@@ -34,4 +36,4 @@ class JobSeeker(models.Model):
     ]
     user = models.OneToOneField(CustomUser, primary_key=True, on_delete=models.CASCADE, related_name='jobseeker')
     gender = models.CharField(max_length=20, choices=GENDER_CHOICES, default=OTHERS)
-    resume = models.FileField(upload_to='resumes/', validators=[validate_file_type], blank=True, null=True)
+    resume = models.FileField(upload_to='resumes/', validators=[FileExtensionValidator(['jpg', 'png', 'jpeg', 'pdf'])], blank=True, null=True)
--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,9 @@
-from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-from users.models import CustomUser, Employer, JobSeeker
 from djoser.serializers import UserCreateSerializer
+from users.models import CustomUser, Employer, JobSeeker
 
 class CustomCreateUserSerializer(UserCreateSerializer):
+    
     def create(self, validated_data):
         instance = super().create(validated_data)
         if instance.user_type == 'Employer':
@@ -11,44 +11,41 @@ class CustomCreateUserSerializer(UserCreateSerializer):
         else:
             JobSeeker.objects.create(user=instance)
         return instance
-        
-
-class EmployerSerializer(ModelSerializer):
-    class Meta:
-        model = Employer
-        fields = ['company', 'location']
-
-class JobseekerSerializer(ModelSerializer):
+    
+class JobSeekerProfileSerializer(ModelSerializer):
     class Meta:
         model = JobSeeker
         fields = ['gender', 'resume']
+        extra_kwargs = {
+            'gender': {'required': False},
+            'resume': {'required': False},
+        }
 
+class EmployerProfileSerializer(ModelSerializer):
+    class Meta:
+        model = Employer
+        fields = ['company', 'location']
+        extra_kwargs = {
+            'company': {'required': False},
+            'location': {'required': False},
+        }
 
 class CustomUserSerializer(ModelSerializer):
-    employer = EmployerSerializer() 
-    jobseeker = JobseekerSerializer()
+    jobseeker = JobSeekerProfileSerializer()
+    employer = EmployerProfileSerializer()
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'user_type', 'first_name', 'last_name', 'email', 'contact_number', 'profile_picture', 'employer', 'jobseeker']
+        fields = ['id', 'username', 'first_name', 'last_name', 'email','user_type', 'jobseeker', 'employer']
         read_only_fields = ['user_type']
 
     def update(self, instance, validated_data):
-        employer_data = validated_data.pop('employer', None)
-        jobseeker_data = validated_data.pop('jobseeker', None)
-
-        instance.username = validated_data.get('username', instance.username)
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.email = validated_data.get('email', instance.email)
-        instance.contact_number = validated_data.get('contact_number', instance.contact_number)
-        instance.profile_picture = validated_data.get('profile_picture', instance.profile_picture)
-        instance.save()
-
-        if employer_data:
-            Employer.objects.update_or_create(user=instance, defaults=employer_data)
-        if jobseeker_data:
-            JobSeeker.objects.update_or_create(user=instance, defaults=jobseeker_data)
-
-        return instance
-
+        jobseeker = validated_data.pop('jobseeker')
+        employer = validated_data.pop('employer')
+        if instance.user_type == 'Employer':
+            serializer = EmployerProfileSerializer()
+            update_instance = serializer.update(instance.employer, employer)
+        elif instance.user_type == 'Jobseeker':
+            serializer = JobSeekerProfileSerializer()
+            update_instance = serializer.update(instance.jobseeker, jobseeker)
+        return super().update(instance, validated_data)
