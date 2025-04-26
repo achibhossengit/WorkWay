ef0e0a6621fce20d610ec11d1cb9134c6efa51ed
achibhossengit <mail.achibhossen@gmail.com>
2025-04-26 15:35:38 +0600

    setup external database

--- WorkWay/settings.py
+++ WorkWay/settings.py
@@ -71,10 +71,21 @@ WSGI_APPLICATION = 'WorkWay.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('dbname'),
+        'USER': config('user'),
+        'PASSWORD': config('password'),
+        'HOST': config('host'),
+        'PORT': config('port')
     }
 }
 
